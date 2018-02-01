5df2c5ee34ef4d51efb9a3e8f83a8dd35e0dff35
Muhamad Omar <mohamadomr33@gmail.com>
2018-02-01 04:46:09 +0200

    ignoring spaces from the input searchString

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.findPhone = function (searchString) {
   var result = [];
   for (var item of phoneList) {
     for (var field of item) {
-      if (field.toString().toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
+      if (field.toString().toLowerCase().replace(/\s+/g, "").indexOf(searchString.replace(/\s+/g, "").toLowerCase()) !== -1) {
         result.push({ 'Retail Branding': item[0], 'Marketing Name': item[1], 'Device': item[2], 'Model': item[3] });
         break;
       }
